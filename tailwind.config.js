18f9411d01bb00357022b181e6e83d9ee2220484
Ananas <ananas@ananas.moe>
2024-10-17 02:59:35 +0200

    fix: make web.dev happy

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "montserrat": ["Montserrat"]
+        "playpen": ["Playpen Sans"],
       },
       colors: {
         pineapple: {
